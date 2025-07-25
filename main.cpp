0e9bb1467cdcad6e852fd98431bcfe7662e65487
Dmitry Bogomaz <d.v.bogomaz@gmail.com>
2025-07-25 16:38:44 +0300

    Первая версия

---
+++ main.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <ctime>
+
+void display(const std::string &msg);
+void display(const std::string &msg,const int value);
+int enterNumber();
+int numberGenerator();
+std::string enterUserName();
+bool checkingNumber(const int intendedNumber, const int numberToCheck);
+
+int main(int argc, char *argv[]) {
+
+    const std::string userName{enterUserName()};
+   
+    display(userName);
+
+    const int intendedNumber{numberGenerator()};
+    display("Загадано число:", intendedNumber);
+
+    while (!checkingNumber(intendedNumber, enterNumber()))
+    {
+        /* code */
+    }
+    
+   
+    
+    return 0;
+}
+
+void display(const std::string &msg) {
+    std::cout << msg << std::endl;
+}
+
+void display(const std::string &msg, const int value) {
+    std::cout << msg << " " << value << std::endl;
+}
+
+int enterNumber() {
+    std::cout << "Enter your guess: ";  
+    int value;
+    std::cin >> value;
+    return value;
+}
+
+int numberGenerator() {
+    const int maxValue{100};
+    std::srand(std::time(nullptr));
+    return std::rand() % maxValue;
+}
+
+std::string enterUserName() {
+    std::cout << "Hi! Enter your name, please: ";
+    std::string userName{};
+    std::cin >> userName;
+    return userName;
+}
+
+bool checkingNumber(const int intendedNumber, const int numberToCheck) {
+    static int att{0};
+    att++;
+    std::cout << "att: " << att << std::endl;
+    bool result{false};
+    if (intendedNumber < numberToCheck) {
+        display("less than ", numberToCheck);
+    }
+    if (intendedNumber > numberToCheck) {
+        display("less than ", numberToCheck);
+    }
+    if (intendedNumber == numberToCheck) {
+        display("you win! attempts =", numberToCheck);
+        result = true;
+    }
+    return result;
+}
